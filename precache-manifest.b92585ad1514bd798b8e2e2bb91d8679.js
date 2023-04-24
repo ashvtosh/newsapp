68b79a37a609e3d321d19637d2bd0244df75505a
Ashutosh <ashutosh1412003@gmail.com>
2023-04-24 11:41:34 +0530

    Updates

---
+++ precache-manifest.b92585ad1514bd798b8e2e2bb91d8679.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a071e21162d73a8a9d28",
+    "url": "/static/css/main.7383a121.chunk.css"
+  },
+  {
+    "revision": "a071e21162d73a8a9d28",
+    "url": "/static/js/main.a071e211.chunk.js"
+  },
+  {
+    "revision": "b5dec1235d8d1d118e4f",
+    "url": "/static/js/1.b5dec123.chunk.js"
+  },
+  {
+    "revision": "794f50e75a127290956c",
+    "url": "/static/js/2.794f50e7.chunk.js"
+  },
+  {
+    "revision": "c13a65875c5c3d089b96",
+    "url": "/static/js/runtime~main.c13a6587.js"
+  },
+  {
+    "revision": "d38c9297629008b8e4e239f96890016b",
+    "url": "/static/media/loading.d38c9297.gif"
+  },
+  {
+    "revision": "323d97b1baaacfc6f947b31fa1f8d8aa",
+    "url": "/index.html"
+  }
+];
